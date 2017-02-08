9b4c8690192a0c3b3d28b25ee22b5b21b06570d1
wcmoton <connor.moton@pcc.edu>
2017-02-08 08:55:06 -0800

    added main method that prints text

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,12 @@ package edu.pcc.cis.helloworld;
  * Created by Hoot on 2/7/2017.
  */
 public class HelloWorld {
+    public void sayHello()
+    {
+        System.out.println("Hello, World");
+        System.out.println("...");
+        System.out.println("Is this thing on?");
+
+
+    }
 }
