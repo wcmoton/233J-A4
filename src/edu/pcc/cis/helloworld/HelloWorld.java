50d7aa738f3ed7c375b782f80582733ac709e5a5
wcmoton <connor.moton@pcc.edu>
2017-02-08 08:57:46 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Hoot on 2/7/2017.
  */
@@ -9,6 +11,7 @@ public class HelloWorld {
         System.out.println("Hello, World");
         System.out.println("...");
         System.out.println("Is this thing on?");
+        System.out.println(new Date());
 
 
     }
